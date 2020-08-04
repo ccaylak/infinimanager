1b4d26e2a8d03a97729bb0eefd56ff5afdc2c8ac
Cem Caylak <cem.caylak@adesso.de>
2020-08-04 12:39:30 +0200

    Add basic header component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <TheHeader />
+  </div>
 </template>
+<script>
+import TheHeader from "@/components/TheHeader";
+export default {
+  components: { TheHeader }
+};
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <b-navbar variant="primary" type="dark">
+      <b-navbar-brand tag="h1" class="mb-0">InfiniManager</b-navbar-brand>
+
+      <b-navbar-nav>
+        <b-nav-item v-b-modal.modal-center>Add dashboard</b-nav-item>
+
+        <b-modal id="modal-center" centered title="Add dashboard" hide-footer>
+          <b-form @submit="onSubmit">
+            <b-form-group
+              label="Dashboard name"
+              label-for="dashboard-name-input"
+            >
+              <b-form-input
+                id="dashboard-name-input"
+                v-model="form.name"
+                type="text"
+                required
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group
+              label="Dashboard description"
+              label-for="dashboard-description-input"
+            >
+              <b-form-input
+                id="dashboard-description-input"
+                v-model="form.description"
+                type="text"
+              ></b-form-input>
+            </b-form-group>
+            <b-button class="mt-3" block type="submit"
+              >Create dashboard</b-button
+            >
+          </b-form>
+        </b-modal>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#"
+          >Logout
+          <b-icon-box-arrow-right></b-icon-box-arrow-right>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import { BIconBoxArrowRight } from "bootstrap-vue";
+
+export default {
+  components: {
+    BIconBoxArrowRight
+  },
+  name: "TheHeader",
+  data() {
+    return {
+      form: {
+        name: null,
+        description: null
+      }
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    }
+  }
+};
+</script>
