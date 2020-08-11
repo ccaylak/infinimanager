06c314e4ef8a881057cf56f3e8015f305f74fa10
Cem Caylak <cem.caylak@adesso.de>
2020-08-11 15:49:49 +0200

    DashboardList modal fix

--- src/components/DeleteDashboardModal.vue
+++ src/components/DeleteDashboardModal.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
     <!-- Delete dashboard modal button -->
-    <b-button v-b-modal.delete class="m-2">
+    <b-button @click="$bvModal.show(name)" class="m-2">
       <b-icon-trash-fill font-scale="1.5"></b-icon-trash-fill>
     </b-button>
     <!-- Delete dashboard modal windows -->
-    <b-modal id="delete" hide-footer title="Delete dashboard">
+    <b-modal v-bind:id="name" hide-footer title="Delete dashboard">
       <!-- Delete dashboard modal content -->
       <div class="d-block text-center">
         <h3>Do you really want to delete {{ name }}?</h3>
       </div>
       <!-- Delete dashboard no button -->
-      <b-button class="mt-3" block>No</b-button>
+      <b-button class="mt-3" @click="$bvModal.hide(name)" block>No</b-button>
       <!-- Delete dashboard yes button -->
-      <b-button class="mt-3" block>Yes</b-button>
+      <b-button class="mt-3" @click="onDelete" block>Yes</b-button>
     </b-modal>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "DeleteDashboardModal",
   props: {
     name: String
+  },
+  methods: {
+    onDelete() {
+      alert("gelösch");
+    }
   }
 };
 </script>
