62c64273290e548143e888de52238d0b9f36c65d
caylak <cem.caylak@adesso.de>
2020-10-01 13:34:54 +0200

    Add 404 page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <h1 class="display-1">
+      404
+    </h1>
+    <h2>
+      Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
+      existed in the first place?
+    </h2>
+    <h4>Let's go <router-link to="/">home</router-link> and try from there.</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound"
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import VueRouter from "vue-router";
 import TheDashboardList from "@/components/dashboard/list/TheDashboardList";
 import EditDashboardItem from "@/components/dashboard/edit/EditDashboardListItem";
 import TheLogin from "@/components/header/TheLogin";
+import PageNotFound from "@/components/PageNotFound";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", component: TheDashboardList },
   { path: "/dashboard/:slug", component: EditDashboardItem },
-  { path: "/login", component: TheLogin }
+  { path: "/login", component: TheLogin },
+  { path: "*", component: PageNotFound }
 ];
 
 const router = new VueRouter({
